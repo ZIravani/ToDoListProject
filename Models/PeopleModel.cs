79c965a585c6db8508f15798db8c9dd618bc7d55
Zahra Iravani <z.iravani121@gmail.com>
2025-03-17 23:47:56 -0700

    Add project files.

---
+++ Models/PeopleModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoListProject.Models
+{
+    public class PeopleModel
+    {
+        public List<string> People { get; set; } = new List<string>();
+
+        public void AddPerson(string person)
+        {
+            if (!string.IsNullOrEmpty(person))
+            {
+                People.Add(person);
+            }
+        }
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListProject.Models
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
